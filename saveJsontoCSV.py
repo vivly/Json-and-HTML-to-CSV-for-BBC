8c5590d175eddc02c59e1e389ae435c0df25fe55
vivly <qmjxt@outlook.com>
2021-08-07 23:05:08 +0800

    1

--- saveJsontoCSV.py
+++ saveJsontoCSV.py
@@ -3,12 +3,12 @@ import pandas as pd
 import json
 
 
-def savejsontocsv(index_path):
+def savejsontocsv(index_path, csv_path):
     if(os.path.exists(index_path)):
         csv_data = pd.read_csv(index_path)
-        data_frame = pd.DataFrame(csv_data)
-        for i in range(0, len(data_frame)):
-            json_path = data_frame.iloc[0,2]
+        csv_data_frame = pd.DataFrame(csv_data)
+        for i in range(0, 1):
+            json_path = csv_data_frame.iloc[0,2]
             with open(json_path, encoding='utf-8') as f:
                 line = f.readline()
                 try:
@@ -51,7 +51,23 @@ def savejsontocsv(index_path):
                     source_domain = d['source_domain']
                 except KeyError:
                     source_domain = ''
-                print(date_publish, title, url, source_domain)
+                try:
+                    main_text = d['maintext']
+                except KeyError:
+                    main_text = ''
+
+                if(os.path.exists(csv_path)):
+                    pass
+                else:
+                    column_list = ['date_publish', 'title', 'description', 'main_text', 'url', 'image_url', 'language',
+                                   'source_domain', 'title_page']
+                    data_list = [date_publish, title, description, main_text, url, image_url, language,
+                                 source_domain, title_page]
+                    data_dict = dict(zip(column_list, data_list))
+                    print(date_publish)
+                    data_frame = pd.DataFrame(columns=column_list)
+                    data_frame = data_frame.append(data_dict, ignore_index=True)
+                    data_frame.to_csv(csv_path, encoding='utf-8')
 
 
 
@@ -61,7 +77,10 @@ def savejsontocsv(index_path):
 if __name__ == '__main__':
     rootPath = r"D:\news_set\data\test"
     index_name_suffix = r'\index.csv'
-    string_list = [rootPath, index_name_suffix]
+    news_csv_suffix = r'\news.csv'
+    string_list_1 = [rootPath, index_name_suffix]
+    string_list_2 = [rootPath, news_csv_suffix]
     temp = ''
-    save_index_path = temp.join(string_list)
-    savejsontocsv(save_index_path)
+    save_index_path = temp.join(string_list_1)
+    save_news_path = temp.join(string_list_2)
+    savejsontocsv(save_index_path, save_news_path)
